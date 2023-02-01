d3ddf122ad93022a810ef68f5103b343fa541d1c
EniV90 <eni_v90@yahoo.com>
2023-02-01 20:58:56 +0100

    bug fix

--- src/containers/Work/Work.jsx
+++ src/containers/Work/Work.jsx
@@ -36,7 +36,7 @@ const Work = () => {
               onClick={() => handleWorkFilter(item)}
               className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
             >
-              
+              {item}
             </div>
 
           ))}
