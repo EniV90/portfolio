c68007166e9ce97340007e8f966055fd385799e4
EniV90 <eni_v90@yahoo.com>
2023-02-02 20:07:46 +0100

    more codes

--- src/containers/Skills/Skills.jsx
+++ src/containers/Skills/Skills.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './Skills.scss'
+import { animate, motion } from 'framer-motion'
+import { AppWrap } from '../../wrapper'
+import { urlFor, client } from '../../client'
+import ReactTooltip from 'react-tooltip'
 
 const Skills = () => {
+
+   useEffect(() => {
+    const query = '*[_type == "works"]'
+
+    client.fetch(query)
+      .then((data) => {
+        setWorks(data);
+        setFilterWork(data)
+      })
+    
+  }, [])
   return (
-    <div>Skills</div>
+    <>
+      <h2 className='head-text'>Skills</h2>
+
+      <div className='app__skills-container'>
+        <motion.div className='app__skills-list'>
+          {skills.map}
+        </motion.div>
+
+      </div>
+    </>
   )
 }
 
