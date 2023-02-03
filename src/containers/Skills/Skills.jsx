ee72cc3b7c3efc42569febc47970abbb00cef0b3
EniV90 <eni_v90@yahoo.com>
2023-02-03 20:57:37 +0100

    added more features

--- src/containers/Skills/Skills.jsx
+++ src/containers/Skills/Skills.jsx
@@ -7,25 +7,47 @@ import ReactTooltip from 'react-tooltip'
 
 const Skills = () => {
 
-   useEffect(() => {
-    const query = '*[_type == "works"]'
+  const [experiences, setExperiences] = useState([]);
+  const [skills, setSkills] = useState([]);
 
-    client.fetch(query)
-      .then((data) => {
-        setWorks(data);
-        setFilterWork(data)
-      })
-    
-  }, [])
+  useEffect(() => {
+    const query = '*[_type == "experiences"]';
+    const skillsQuery = '*[_type == "skills"]';
+
+    client.fetch(query).then((data) => {
+      setExperiences(data);
+    });
+
+    client.fetch(skillsQuery).then((data) => {
+      setSkills(data);
+    });
+  }, []);
   return (
     <>
       <h2 className='head-text'>Skills</h2>
 
       <div className='app__skills-container'>
         <motion.div className='app__skills-list'>
-          {skills.map}
+          {skills.map((skill) => (
+            <motion.div
+              whileInView={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              className="app__skills-item app__flex"
+              key={skill.name}
+            >
+              <div className='app__flex' style={{ backgroundColor: skill.bgColor }}>
+                <img src={urlFor(skill.icon)} alt={skill.name} />
+              </div>
+              <p className='p-text'>{skill.name}</p>
+            </motion.div>
+          ))}
         </motion.div>
-
+        <motion.div className='app__skills-exp'>
+          {experiences.works.map((work) => (
+            <>
+            </>
+          ))}
+        </motion.div>
       </div>
     </>
   )
