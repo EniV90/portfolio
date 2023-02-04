06f482e5707cfa6d763553bc32d368c3d7494730
EniV90 <eni_v90@yahoo.com>
2023-02-04 13:03:02 +0100

    code updates

--- src/containers/Skills/Skills.jsx
+++ src/containers/Skills/Skills.jsx
@@ -3,11 +3,11 @@ import './Skills.scss'
 import { animate, motion } from 'framer-motion'
 import { AppWrap } from '../../wrapper'
 import { urlFor, client } from '../../client'
-import ReactTooltip from 'react-tooltip'
+import {Tooltip as ReactTooltip} from 'react-tooltip'
 
 const Skills = () => {
 
-  const [experiences, setExperiences] = useState([]);
+  const [experience, setExperience] = useState([]);
   const [skills, setSkills] = useState([]);
 
   useEffect(() => {
@@ -15,7 +15,7 @@ const Skills = () => {
     const skillsQuery = '*[_type == "skills"]';
 
     client.fetch(query).then((data) => {
-      setExperiences(data);
+      setExperience(data);
     });
 
     client.fetch(skillsQuery).then((data) => {
@@ -42,12 +42,44 @@ const Skills = () => {
             </motion.div>
           ))}
         </motion.div>
-        <motion.div className='app__skills-exp'>
-          {experiences.works.map((work) => (
-            <>
+        <div className='app__skills-exp'>
+          {experience.map((experience) => (
+              <motion.div
+                  className='app__skills-exp-item'
+                  key={experience.year}
+              >
+                  <div className='app__skills-exp-year'>
+                      <p className='bold-text'>{ experience.year }</p>
+                  </div>
+                  <motion.div className='app__skills-exp-works'>
+                      {experience.works.map((work) => (
+                          <>
+                <motion.div
+              whileInView={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              className="app__skills-exp-work"
+                data-tip
+                data-for={work.name}
+              key={work.name}
+              >
+                <h4 className='bold-text'>{work.name}</h4>
+                <p className='p-text'>{work.company}</p>
+              </motion.div>
+              <ReactTooltip
+                id={work.name}
+                effect="solid"
+                arrowColor="#fff"
+                className="skills-tooltip"
+              >
+                {work.description}
+              </ReactTooltip>
             </>
+                      ))}
+                  </motion.div>
+            </motion.div>
+            
           ))}
-        </motion.div>
+        </div>
       </div>
     </>
   )
